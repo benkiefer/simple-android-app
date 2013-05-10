fb544fde53d92813551b99882d3d3489a2e117f5
Ben Kiefer <kieferfam@msn.com>
2013-05-09 20:15:09 -0500

    Ben: working on getting the github api hooked up:

--- src/org/burgers/github_android/MainActivity.java
+++ src/org/burgers/github_android/MainActivity.java
@@ -2,14 +2,24 @@ package org.burgers.github_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RepositoryService service = new RepositoryService();
+        try {
+            for (Repository repository : service.getRepositories("benkiefer")) {
+                System.out.println(repository.getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.main);
     }
 }
