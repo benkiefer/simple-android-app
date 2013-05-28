510be0e0fc330345e6185d9cf31d871133fe5224
Ben Kiefer <kieferfam@msn.com>
2013-05-28 12:02:05 -0500

    Ben: taking care of an error condition where we do not find repositories for the user you search for

--- src/org/burgers/github_android/RepositoryListActivity.java
+++ src/org/burgers/github_android/RepositoryListActivity.java
@@ -5,33 +5,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 import org.eclipse.egit.github.core.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class RepositoryListActivity extends ListActivity {
     public static final String USER_NAME = "user_name";
 
     @Override
-     public void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.repository_list);
 
         String username = getIntent().getStringExtra(USER_NAME);
 
         List<Repository> repositories = null;
         try {
-            repositories = new GetRepositoriesTask(this).execute(username).get();
+            repositories = new GetRepositoriesTask(this).execute(username).get(10, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             repositories = new ArrayList<Repository>();
             e.printStackTrace();
         } catch (ExecutionException e) {
             repositories = new ArrayList<Repository>();
-            e.printStackTrace();
+        } catch (TimeoutException e) {
+            repositories = new ArrayList<Repository>();
         }
 
-        setListAdapter(new RepositoryListAdapter(this, R.layout.row_layout, repositories));
+        if (!repositories.isEmpty()) {
+            setListAdapter(new RepositoryListAdapter(this, R.layout.row_layout, repositories));
+        } else {
+            TextView emptyView = (TextView) getListView().getEmptyView();
+            emptyView.setText("There are no repositories for user: " + username);
+        }
     }
 
     @Override
